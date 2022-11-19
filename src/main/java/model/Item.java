141de9273769e370b6901abc4c761860cbf9f6ed
mateusz <mateusz.soltys@protonmail.com>
2022-11-19 21:47:12 +0100

    Hibernate annotations

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -1,6 +1,12 @@
 package model;
 
 import lombok.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import java.time.LocalDateTime;
 
@@ -9,16 +15,24 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name="Items")
 public class Item {
 
+    @Id
+    @Column(name="key")
     private String key;
 
+    @Column(name="category")
     private String category;
 
+    @Column(name="name")
     private String name;
 
+    @Column(name="district")
     private String district;
 
+    @Column(name="createDate")
     private LocalDateTime createDate;
 
 }

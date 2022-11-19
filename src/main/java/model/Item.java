363953eb9f508a992d7d6ed7f768265b965f0362
mateusz <mateusz.soltys@protonmail.com>
2022-11-19 19:41:28 +0100

    Added all args constructor for item

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -1,15 +1,13 @@
 package model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class Item {
 

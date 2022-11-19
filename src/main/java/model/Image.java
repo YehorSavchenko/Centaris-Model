37be290fbb4e4760d2fa45e7af63a19ec5a7b9ee
mateusz <mateusz.soltys@protonmail.com>
2022-11-20 00:55:40 +0100

    Added image entry

---
+++ src/main/java/model/Image.java
@@ -0,0 +1,21 @@
+package model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name="images")
+public class Image {
+    @Column
+    public Long personId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+}

efdf717e50fe9f15769517956cc86d41638a9c55
sasha-ishchuk <alexa.ishchuk@gmail.com>
2022-11-20 00:39:10 +0100

    add User entity

---
+++ src/main/java/model/User.java
@@ -0,0 +1,32 @@
+package model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String firstName;
+
+    @Column(nullable = false, length = 20)
+    private String lastName;
+
+    @Column(nullable = false, length = 45, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 64, unique = true)
+    private String password;
+}
